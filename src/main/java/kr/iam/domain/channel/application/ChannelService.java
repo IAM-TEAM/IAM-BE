1fd12b7e7a0f36d99d4d4dc9eabd4bb372723594
Lee <lych0918@gachon.ac.kr>
2024-05-03 16:08:23 +0900

    feat: Channel controller, service, repo, dto class 생성

---
+++ src/main/java/kr/iam/domain/channel/application/ChannelService.java
@@ -0,0 +1,16 @@
+package kr.iam.domain.channel.application;
+
+import kr.iam.domain.channel.dao.ChannelRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+public class ChannelService {
+
+    private final ChannelRepository channelRepository;
+}
